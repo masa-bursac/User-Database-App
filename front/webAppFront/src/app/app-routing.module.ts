7e7e94e216c63da0fc9f32d6a84276a7383c1cd9
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2024-11-08 09:44:38 +0100

    front basic landing, home page, navbar and registration components implemented

--- front/webAppFront/src/app/app-routing.module.ts
+++ front/webAppFront/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page/home-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'welcome', component: LandingPageComponent},
+  {path: '', pathMatch:'full', redirectTo: 'welcome'},
+  {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/webAppFront/src/app/app.module.ts
+++ front/webAppFront/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page/home-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LandingPageComponent,
+    NavBarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
