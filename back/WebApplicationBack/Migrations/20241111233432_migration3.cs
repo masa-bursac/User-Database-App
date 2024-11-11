b05bfc1ab4d23012077812d0af72aa3922c0bb19
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2024-11-12 00:35:33 +0100

    back: add migration with hashed password for admin

---
+++ back/WebApplicationBack/Migrations/20241111233432_migration3.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationBack.Migrations
+{
+    public partial class migration3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAEAACcQAAAAEAzjJqJFLYnuSy+pPFLyVi+aFpl96enyKanRGZxDNwJJDy/qf2+FDuO8f4T7sdfWog==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "admin");
+        }
+    }
+}
